f10ddad9233717b6db8842e8ac6877339acf7788
Zikokiko2 <92632225+Zikokiko2@users.noreply.github.com>
2023-04-12 13:38:25 +0200

    Model to apps

---
+++ ToDoLogic/Model/Calendar.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+
+namespace ToDoLogic.Model
+{
+    public class Calendar : IEnumerable<TaskModel>
+    {
+        public List<TaskModel> taskList = new List<TaskModel>();
+        public Calendar model;
+
+        public Calendar(Calendar model)
+        {
+            this.model = model;
+        }
+
+        //crud
+        public void AddTask(TaskModel model)
+        {
+            taskList.Add(model);
+        }
+
+        public bool RemoveTask(TaskModel model)
+        {
+            return taskList.Remove(model);
+        }
+
+        public int NumOfTasks
+        {
+            get
+            {
+                return taskList.Count;
+            }
+        }
+
+        public TaskModel this[int index]
+        {
+            get
+            {
+                return taskList[index];
+            }
+        }
+
+        public IEnumerator<TaskModel> GetEnumerator()
+        {
+            return taskList.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator)this.GetEnumerator();
+        }
+    }
+}
---
+++ ToDoLogic/Model/TaskModel.cs
@@ -0,0 +1,44 @@
+﻿namespace ToDoLogic.Model
+{
+
+    public enum PriorityLevel : byte
+    {
+        Low,
+        Medium,
+        High
+    }
+
+    public class TaskModel
+    {
+        public string Duty { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime CreationDate { get; set; }
+        public PriorityLevel Priority { get; set; }
+        public bool IsDone { get; set; }
+
+        public TaskModel(string duty , DateTime creationDate , DateTime date , PriorityLevel priority , bool isDone = false)
+        {
+            this.Duty = duty;
+            this.CreationDate = creationDate;
+            this.Date = date;
+            this.Priority = priority;
+            this.IsDone = isDone;
+        }
+
+        public override string ToString()
+        {
+            return Duty + ", Creation Time " + CreationDate.ToString() + ", Date: " + Date.ToString() +
+                ", priority: " + Priority.ToString() + ", is task done: " + (IsDone ? "done" : "undone");
+        }
+    }
+
+    public class TaskModelForConsole
+    {
+        public int ID { get; set; }
+        public string? Duty { get; set; }
+        public DateTime Date { get; set; }
+        public PriorityLevel Priority { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
+
