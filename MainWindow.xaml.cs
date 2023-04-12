5e89230b2ff3f61871b3f3c8ea262dcc159c2dbd
Zikokiko2 <92632225+Zikokiko2@users.noreply.github.com>
2023-04-12 13:41:16 +0200

    wpfWithoutMvvm

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfWithoutmvvm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender , RoutedEventArgs e)
+        {
+            string text = InputTextBox.Text;
+            TextBlock textBlock = new TextBlock();
+            textBlock.Text = text;
+            ListBox.Items.Add(textBlock);
+        }
+
+        private void DeleteButton_Click(object sender , RoutedEventArgs e)
+        {
+            if(ListBox.SelectedItem != null)
+            {
+                ListBox.Items.Remove(ListBox.SelectedItem);
+            }
+        }
+    }
+}
